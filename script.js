9487f02dfbc304d4671eade7ecd93570ddf458b1
Rounak <rounakkumar040@gmail.com>
2023-05-31 22:56:08 +0530

    added new features

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 //Selecting elemenets
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -20,6 +22,14 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 btnRoll.addEventListener('click', function () {
   //Generating random number
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -35,10 +45,26 @@ btnRoll.addEventListener('click', function () {
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
 
-    current0El.textContent = currentScore; //
+    // current0El.textContent = currentScore; //
   } else {
     //Switch to next player
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  if (scores[activePlayer] >= 10) {
+    document
+      .querySelector(`player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`player--${activePlayer}`)
+      .classList.add('player--active');
+  }
+
+  switchPlayer();
 });
